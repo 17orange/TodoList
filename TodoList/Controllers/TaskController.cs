2545d5cebc8e7bc0f4ec93e30925c7d93e594394
Brad Patton <bradley.patton@gmail.com>
2015-02-26 13:09:28 -0500

    Initial commit of the default MVC addition of the TodoTask table.

---
+++ TodoList/Controllers/TaskController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TodoList.Models;
+
+namespace TodoList.Controllers
+{
+    public class TaskController : Controller
+    {
+        private TodoTaskDBContext db = new TodoTaskDBContext();
+
+        // GET: Task
+        public ActionResult Index()
+        {
+            return View(db.TodoTasks.ToList());
+        }
+
+        // GET: Task/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoTask todoTask = db.TodoTasks.Find(id);
+            if (todoTask == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoTask);
+        }
+
+        // GET: Task/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Task/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TodoTaskID,userID,name,description,creationTime,deadlineTime,statusID,isPublic")] TodoTask todoTask)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TodoTasks.Add(todoTask);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(todoTask);
+        }
+
+        // GET: Task/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoTask todoTask = db.TodoTasks.Find(id);
+            if (todoTask == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoTask);
+        }
+
+        // POST: Task/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TodoTaskID,userID,name,description,creationTime,deadlineTime,statusID,isPublic")] TodoTask todoTask)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(todoTask).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todoTask);
+        }
+
+        // GET: Task/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoTask todoTask = db.TodoTasks.Find(id);
+            if (todoTask == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoTask);
+        }
+
+        // POST: Task/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TodoTask todoTask = db.TodoTasks.Find(id);
+            db.TodoTasks.Remove(todoTask);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TodoList/Models/TaskModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace TodoList.Models
+{
+    // this class defines our task model
+    public class TodoTask
+    {
+        // ID for the task itself
+        public int TodoTaskID { get; set; }
+
+        // ID of the creator of this task
+        public string userID { get; set; }
+
+        // short description of the task
+        public string name { get; set; }
+
+        // full description of the task
+        public string description { get; set; }
+
+        // creation timestamp
+        public DateTime creationTime { get; set; }
+
+        // deadline timestamp
+        public DateTime deadlineTime { get; set; }
+
+        // the status of this task
+        public int statusID { get; set; }
+
+        // whether other users can see this task or not
+        public bool isPublic { get; set; }
+    }
+
+    public class TodoTaskDBContext : DbContext
+    {
+        public DbSet<TodoTask> TodoTasks { get; set; }
+    }
+}
